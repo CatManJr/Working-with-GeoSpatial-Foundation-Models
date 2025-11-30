09c6727a2f514322b4a257c0fcace262ac76d73f
CatManJr <2396242639@qq.com>
2025-11-30 17:41:41 -0500

    Update: bug fixed in run_accessibility.sh

--- pop_exposure/run_accessibility.sh
+++ pop_exposure/run_accessibility.sh
@@ -2,10 +2,8 @@
 
 # Run accessibility analysis for different bandwidths and different number of agents
 for bandwidth in 250 500 1000 2500
-do
-    for num_agents in 1 2 3 4 5
     do
-        echo "Running accessibility analysis with bandwidth: $bandwidth and num_agents: $num_agents"
-        uv run pop_exposure/accessibility.py --bandwidth $bandwidth --num_agents $num_agents
+        echo "Running accessibility analysis with bandwidth: $bandwidth
+        uv run pop_exposure/accessibility.py --bandwidth $bandwidth
     done
 done
